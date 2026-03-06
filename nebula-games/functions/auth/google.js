cb43319a012b5ab2d39138ab1de8385213990780
suicad005199-crypto <suicad005199@gmail.com>
2026-03-06 18:44:55 +0800

    Update google.js

--- nebula-games/functions/auth/google.js
+++ nebula-games/functions/auth/google.js
@@ -1,1 +1,4 @@
-
+export async function onRequest(context) {
+    // 這裡處理 Google OAuth 回傳的 code 並更新 Supabase 中的 email
+    return new Response(JSON.stringify({ message: "Google 綁定 API 運作中" }), { headers: { "Content-Type": "application/json" } });
+}
