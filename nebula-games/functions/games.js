92df769fb5966eea6d8d81ae8f06c4b2d787dfa0
suicad005199-crypto <suicad005199@gmail.com>
2026-03-06 18:40:26 +0800

    Update games.js

--- nebula-games/functions/games.js
+++ nebula-games/functions/games.js
@@ -1,1 +1,7 @@
-
+export async function onRequest(context) {
+    const { env } = context;
+    const res = await fetch(`${env.SUPABASE_URL}/rest/v1/games?is_published=eq.true&order=sort_order.asc`, {
+        headers: { "apikey": env.SUPABASE_SERVICE_ROLE_KEY, "Authorization": `Bearer ${env.SUPABASE_SERVICE_ROLE_KEY}` }
+    });
+    return new Response(await res.text(), { headers: { "Content-Type": "application/json" } });
+}
