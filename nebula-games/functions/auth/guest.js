79156525a49ad9b8f87fd71fc0e941044cc1da5c
suicad005199-crypto <suicad005199@gmail.com>
2026-03-10 13:13:56 +0800

    Delete nebula-games directory

--- nebula-games/functions/auth/guest.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest() {
-    const guest = {
-        id: `guest_${Math.random().toString(36).substr(2, 9)}`,
-        display_name: "訪客玩家",
-        is_guest: true
-    };
-    return new Response(JSON.stringify(guest), { headers: { "Content-Type": "application/json" } });
-}
